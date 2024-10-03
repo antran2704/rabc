a04aa8c9d00de30c5e189824c8050fd5224c5de2
an tran <antran.teachhert@gmail.com>
2024-10-03 21:19:36 +0700

    test timeline calender

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,163 @@
-import { Link } from "react-router-dom";
-import useBlocker from "../hooks/useBlocker";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import bootstrap5Plugin from "@fullcalendar/bootstrap5";
 
-const HomePage = () => {
-  const { isBlocker, onSetBlocker } = useBlocker();
-  // Block navigating elsewhere when data has been entered into the input
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useState } from "react";
 
-  console.log("isBlocker", isBlocker);
+const events = [
+  {
+    title: "Meeting",
+    start: new Date('2024-10-03T11:00:00'),
+    end: new Date('2024-10-03T16:00:00'),
+    // display: 'background'
+  },
+];
 
+function renderEventContent(eventInfo) {
+  console.log(eventInfo);
   return (
     <div>
-      <div>
-        this is Home page
-        <Link to={"/about"}>About</Link>
-      </div>
+      <p>{eventInfo.timeText}</p>
+      <p>{eventInfo.event.title}</p>
+    </div>
+  );
+}
 
-      <button onClick={() => onSetBlocker(!isBlocker)}>
-        Onclick to turn on Blocker
-      </button>
+function Sidebar({
+  weekendsVisible,
+  handleWeekendsToggle,
+  currentEvents = [],
+}) {
+  return (
+    <div className="demo-app-sidebar">
+      <div className="demo-app-sidebar-section">
+        <h2>Instructions</h2>
+        <ul>
+          <li>Select dates and you will be prompted to create a new event</li>
+          <li>Drag, drop, and resize events</li>
+          <li>Click an event to delete it</li>
+        </ul>
+      </div>
+      <div className="demo-app-sidebar-section">
+        <label>
+          <input
+            type="checkbox"
+            checked={weekendsVisible}
+            onChange={handleWeekendsToggle}
+          ></input>
+          toggle weekends
+        </label>
+      </div>
+      <div className="demo-app-sidebar-section">
+        <h2>All Events ({currentEvents.length})</h2>
+        <ul>
+          {currentEvents.map((event, index) => (
+            <SidebarEvent key={index} event={event} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function SidebarEvent({ event }) {
+  return (
+    <li>
+      <b>{/* {event.start} */}</b>
+      <i>{event.title}</i>
+    </li>
+  );
+}
+
+let autoId = 0;
+
+const HomePage = () => {
+  const [weekendsVisible, setWeekendsVisible] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  function handleWeekendsToggle() {
+    setWeekendsVisible(!weekendsVisible);
+  }
+
+  function handleDateSelect(selectInfo) {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: autoId++,
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  }
+
+  function handleEventClick(clickInfo) {
+    console.log(clickInfo.event);
+
+    // if (
+    //   confirm(
+    //     `Are you sure you want to delete the event '${clickInfo.event.title}'`
+    //   )
+    // ) {
+    //   clickInfo.event.remove();
+    // }
+    clickInfo.event.setProp("title", "new title");
+  }
+
+  function handleEvents(events) {
+    setCurrentEvents(events);
+  }
+
+  return (
+    <div className="demo-app">
+      <Sidebar
+        weekendsVisible={weekendsVisible}
+        handleWeekendsToggle={handleWeekendsToggle}
+        currentEvents={currentEvents}
+      />
+      <div className="demo-app-main">
+        <FullCalendar
+          plugins={[
+            dayGridPlugin,
+            timeGridPlugin,
+            interactionPlugin,
+            bootstrap5Plugin,
+          ]}
+          defaultRangeSeparator=" - "
+          themeSystem="bootstrap5"
+          slotDuration={'00:15:00'}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={weekendsVisible}
+          initialEvents={events} // alternatively, use the `events` setting to fetch from a feed
+          select={handleDateSelect}
+          eventContent={renderEventContent} // custom render function
+          eventClick={handleEventClick}
+          eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+          /* you can update a remote database when these fire:
+          eventAdd={function(){}}
+          eventChange={function(){}}
+          eventRemove={function(){}}
+          */
+        />
+      </div>
     </div>
   );
 };
